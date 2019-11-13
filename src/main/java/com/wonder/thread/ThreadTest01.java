e3370ebaed08c7200a3d01ce19bd5c270ac8dbf8
wangjinrong <wangjinrong@bogon>
2019-11-13 22:57:07 +0800

    first commit

---
+++ src/main/java/com/wonder/thread/ThreadTest01.java
@@ -0,0 +1,25 @@
+package com.wonder.thread;
+
+//依靠异常处理机制，3s后打断线程的休眠
+public class ThreadTest01 {
+  public static void main(String[] args) throws InterruptedException {
+      Thread t=new Thread(new Processor1());
+      t.setName("t");
+      t.start();
+//      Thread.sleep(3000);
+//      t.interrupt();//打断t的休眠
+  }
+}
+
+class Processor1 implements Runnable {
+	public void run() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; i < 4; i++) {
+			System.out.println(Thread.currentThread().getName() + "-->" + i);
+		}
+	}
+}
